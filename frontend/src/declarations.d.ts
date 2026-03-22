6d594d4fdd88fe4e9e3539f2946bb87f27fdcf54
amandamigliorini <amanda.migliorini@hotmail.com>
2026-03-21 21:12:07 -0300

    axios updated

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -2,3 +2,9 @@ declare module "*.webp";
 declare module "*.png";
 declare module "*.svg";
 declare module "*.module.css";
+
+interface ImportMeta {
+    readonly env: {
+      readonly VITE_API_URL: string;
+    };
+}
